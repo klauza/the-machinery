e99f7b7be35bc469f11610c4b0520fde47a6e3ba
klauza <klauza.uk@gmail.com>
2019-07-23 13:40:56 +0100

    create logReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'; 
+import logReducer from './logReducer';
+
 
 export default combineReducers({  //takes an object with all reducers
-
+  log: logReducer
 });
