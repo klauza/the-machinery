3c870cc9c5e509da13778b7351756cc7a172536d
klauza <klauza.uk@gmail.com>
2019-07-23 11:42:34 +0100

    displaying technicians

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 
 import 'materialize-css/dist/css/materialize.min.css';
@@ -23,6 +24,7 @@ const App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
