1324f4e66936e8e19ac98ffc7b0a4107b0276456
klauza <klauza.uk@gmail.com>
2019-07-25 16:47:45 +0100

    added background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useEffect} from 'react';
+import './App.css';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
